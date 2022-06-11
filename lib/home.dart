d1a947bffbbd728a81a56e427f526b0ed72f1600
anasahmed01 <zainkhilji2323@gmail.com>
2022-06-11 18:00:55 +0500

    add_1st_screen

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,55 @@ class Homeapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(100.0),
+            child: Icon(
+              Icons.lightbulb_outline_sharp,
+              color: Color.fromARGB(255, 0, 30, 180),
+              size: 100,
+            ),
+          ),
+          Center(
+            child: Column(
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 0, 30, 180)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        side: const BorderSide(
+                          color: Color.fromARGB(255, 29, 9, 139),
+                          width: 2.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'INSTANT PAY',
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900),
+                  ),
+                  onPressed: () {},
+                ),
+                const Text(
+                  'Your Perfect Payment Partner',
+                  style: TextStyle(color: Colors.white),
+                ),
+                const Icon(
+                  Icons.more_horiz,
+                  color: Colors.white,
+                  size: 70,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
